db7575a9c1ea548d49146c411df675d72f412739
daniel <yoojeenkim@gmail.com>
2021-10-29 20:27:24 -0400

    added route for homepage

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const homeRoute = require('./home-route');
+
+router.use('/', homeRoute);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
